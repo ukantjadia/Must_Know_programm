64cbea08d64c2c0d617de6981493964944db9512
UkantJadia <74947441+UserCtf777@users.noreply.github.com>
2023-02-21 06:53:24 +0530

    error in java code

---
+++ Recursion/recursionSum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class recursionSum{
+    public int sumNumber(int n){
+        if( n == 1)
+            return 1;
+        else
+            return n + sumNumber(n-1);
+    }
+
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the limit till sum to be done: ");
+        int n = sc.nextInt();
+        sumNumber obj = new sumNumber();
+        int sum = obj.sumnNumber(n);
+        System.out.println("The sum of number is : "+sum);
+    }
+}
