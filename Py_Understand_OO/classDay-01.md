f550cf2ef90fd768be009cd42e79ee6e16826626
UkantJadia <74947441+UserCtf777@users.noreply.github.com>
2023-02-23 18:42:39 +0530

    build: added python class day-01

---
+++ Py_Understand_OO/classDay-01.md
@@ -0,0 +1,13 @@
+#
+
+- method -> a function that is associated with a class
+- Class -> a class we can use to create a blueprint of each employee, So we don't have to create it from scratch for employee
+- instance variable -> contain data that is create to each instance
+- `__init__` -> this is class way of creating the instance variable for 
+            <br> think of  `__init__` as a initializer and if you are with other language background take it as a constructor
+- method -> adding a functionallity
+- Each method in a class automatically takes instance as a first argument and always called that self
+
+### Difference between a class and an instance of a class
+class - class is a blueprint for creating instances for each employee
+instance - creating a user of any class like employee
---
+++ Py_Understand_OO/classDay-01.py
@@ -0,0 +1,33 @@
+"""
+read: classDay-01.md
+
+base: a application for a company in python for its employee 
+"""
+
+# It is easy to cerate a class in python 
+
+class Employee:
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.'+last+'@company.com'
+        
+
+emp_1 = Employee('Ukant','Jadia',30000)
+emp_2 = Employee('Rohit','Jadia',40000) # Employee objects and class
+
+## to check that they both are unique print them
+# print(f'{emp_1}\n{emp_2}')
+
+
+## Instance variable 
+## this is manually without using class
+## It will be unique to each instance of a class
+# emp_1.first = 'Ukant'
+# emp_1.last = 'Jadia'
+# emp_1.email = 'Ukant.Jadia@company.com'
+# emp_1.pay = 300000
+
+
+print(f'{emp_1.email}\n{emp_2.email}')
