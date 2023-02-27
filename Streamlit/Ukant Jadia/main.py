2e920562735ae6b92f5d103c7043a9dd2b6d594b
UkantJadia <74947441+UserCtf777@users.noreply.github.com>
2023-02-27 06:04:07 +0530

    build: D CV update and dsa files added

--- Streamlit/Ukant Jadia/main.py
+++ Streamlit/Ukant Jadia/main.py
@@ -7,7 +7,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Ukantjadia.pdf"
-profile_pic = current_dir / "assets" / "ukant.jpg"
+profile_pic = current_dir / "assets" / "ukant.png"
 
 ## gerenral settings
 PAGE_TITLE = "Digital CV | Ukant Jadia"
